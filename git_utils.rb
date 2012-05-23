a50d631e0c907b59c8bbc0ec6213acea4afcd8b6
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-05-23 13:27:49 -0600

    Updated the git utilities

--- git_utils.rb
+++ git_utils.rb
@@ -1,12 +1,17 @@
-def exit_if_on_the_master_branch
-  exit_if_on_branch('master')
+def exit_if_on_the_branch(branch)
+  exit_if_branch_condition_fails(lambda{|expression,status|expression =~ status},branch,lambda{|item|  "You cant run this command on the branch #{item}"})
 end
 
-def exit_if_on_branch(branch)
-  status = `git branch`
+def exit_if_not_on_the_branch(branch)
+  exit_if_branch_condition_fails(lambda{|expression,status|expression !~ status},branch,lambda{|item| "You need to run this on the branch #{item}"})
+end
 
-  if (%r/\* #{branch}/ =~ status)
-    puts "You cant run this command on the branch #{branch}"
+def exit_if_branch_condition_fails(specification_block,branch,message_block)
+  status = run_git_command("branch",true)
+  match_expression = %r/\* #{branch}/
+
+  if (specification.call(match_expression,status))
+    puts message_block.call(branch)
     exit
   end
 end
@@ -21,24 +26,10 @@ def run_git_command(command,capture_ouptut = false)
 end
 
 def checkout(branch_name)
-  `git checkout -b #{branch_name}`
-  `git checkout #{branch_name}`
+  run_git_command("checkout -b #{branch_name}")
+  run_git_command("checkout #{branch_name}")
 end
 
-def exit_if_not_on_the_branch(branch)
-
-  status = `git branch`
-
-  if (%r/\* #{branch}/ !~ status)
-    puts "You need to run this on the #{branch} branch"
-    exit
-  end
-
-end
-
-def exit_if_not_on_the_master_branch
-  exit_if_not_on_the_branch 'master'
-end
 
 def pick_item_from(items,prompt)
   items.each_with_index{|item,index| p "#{index + 1} - #{item}\n"}
